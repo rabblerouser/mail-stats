872c008f5ab5d1390d25fe9fc2bc42995bdc0ab0
Cam Jackson <camjackson89@gmail.com>
2017-11-17 12:52:53 +0800

    Add default config values and testing to the backend

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  streamName: process.env.STREAM_NAME,
-  listenerAuthToken: process.env.LISTENER_AUTH_TOKEN,
-  archiveBucket: process.env.ARCHIVE_BUCKET,
+  streamName: process.env.STREAM_NAME || 'rabblerouser_stream',
+  listenerAuthToken: process.env.LISTENER_AUTH_TOKEN || 'secret',
+  archiveBucket: process.env.ARCHIVE_BUCKET || 'rr-mail-archive',
 
-  region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION || 'ap-southeast-2',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'fake',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'also-fake',
 
   kinesisEndpoint: process.env.KINESIS_ENDPOINT,
   s3Endpoint: process.env.S3_ENDPOINT,
