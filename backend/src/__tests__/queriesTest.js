d4e5275bdd9813f53226b85f16a780457b0e24e1
Cam Jackson <camjackson89@gmail.com>
2017-11-18 22:51:48 +0800

    Sort the campaigns by date descending, in the API

--- backend/src/__tests__/queriesTest.js
+++ backend/src/__tests__/queriesTest.js
@@ -19,4 +19,20 @@ describe('queries', () => {
 
     expect(res.send).to.have.been.calledWith({ campaigns: [campaign] });
   });
+
+  it('sorts the campaigns by date, most-recent first, with missing dates last', () => {
+    store.addCampaign({ id: '1', date: '2016-11-16T14:14:59.482Z' });
+    store.addCampaign({ id: '2', date: '2017-11-16T14:14:59.482Z' });
+    store.addCampaign({ id: '3' });
+    store.addCampaign({ id: '4', date: '2015-11-16T14:14:59.482Z' });
+
+    const res = { send: sinon.spy() };
+    getCampaigns(null, res);
+
+    const sentCampaigns = res.send.args[0][0].campaigns;
+    expect(sentCampaigns[0].id).to.eql('2');
+    expect(sentCampaigns[1].id).to.eql('1');
+    expect(sentCampaigns[2].id).to.eql('4');
+    expect(sentCampaigns[3].id).to.eql('3');
+  });
 });
--- backend/src/queries.js
+++ backend/src/queries.js
@@ -1,6 +1,11 @@
 module.exports = (store) => {
   const getCampaigns = (req, res) => {
-    res.send({ campaigns: store.campaigns });
+    const campaigns = store.campaigns.sort((a, b) => {
+      if (!a.date) return 1;
+      if (!b.date) return -1;
+      return Date.parse(b.date) - Date.parse(a.date);
+    });
+    res.send({ campaigns });
   };
 
   return { getCampaigns };
