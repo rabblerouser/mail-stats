a7238e6ead09c7b14e1122924a5d292621c9dd6c
Cam Jackson <camjackson89@gmail.com>
2017-11-18 11:51:27 +0800

    Add logging to the CI script

--- ci.sh
+++ ci.sh
@@ -6,22 +6,29 @@ BUILD_NUMBER="${2:-latest}"
 
 case "$COMMAND" in
   "build")
+    echo 'Building backend image'
     cd ./backend && docker build --pull -t rabblerouser/mail-stats:${BUILD_NUMBER} .
+    echo 'Building frontend image'
     cd ../frontend && docker build --pull -t rabblerouser/mail-stats-frontend:${BUILD_NUMBER} .
     cd ..
-    # Build frontend assets and make them available outside the docker container
+    echo 'Building frontend static assets'
+    docker rm assets || true
     docker run --name assets rabblerouser/mail-stats-frontend:${BUILD_NUMBER} yarn build
     docker cp assets:/app/build assets
     ;;
   "test")
+    echo 'Running backend linting and tests'
     docker run rabblerouser/mail-stats:${BUILD_NUMBER} yarn lint
     docker run rabblerouser/mail-stats:${BUILD_NUMBER} yarn test
+    echo 'Running frontend linting and tests'
     docker run rabblerouser/mail-stats-frontend:${BUILD_NUMBER} yarn lint
     docker run -e CI=true rabblerouser/mail-stats-frontend:${BUILD_NUMBER} yarn test
     ;;
   "push")
     docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
+    echo 'Pushing backend image'
     docker push rabblerouser/mail-stats:${BUILD_NUMBER}
+    echo 'Pushing frontend image (for dev purposes only)'
     docker push rabblerouser/mail-stats-frontend:${BUILD_NUMBER}
     ;;
   "usage")
