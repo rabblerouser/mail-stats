54830d909784406b5efaea1b0a8e4cf066c57f4f
Cam Jackson <camjackson89@gmail.com>
2017-11-19 10:06:31 +0800

    Add logging with morgan and winston

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const streamClient = require('./streamClient');
 const Store = require('./store');
 const queries = require('./queries');
+const logger = require('./logger');
 
 const app = express();
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
 
 const store = new Store();
 streamClient.on('send-email', campaign => (
@@ -18,5 +24,5 @@ app.get('/api/campaigns', getCampaigns);
 
 const port = process.env.PORT;
 app.listen(port, () => {
-  console.log('Listening on port', port);
+  logger.info('Listening on port', port);
 });
