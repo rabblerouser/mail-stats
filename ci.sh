1a10be8f9c8f84bd14fd67fcbf1c9a6889361ac3
Cam Jackson <camjackson89@gmail.com>
2018-06-03 22:53:18 +1000

    Make the CI config/script much simpler

--- ci.sh
+++ ci.sh
@@ -1,61 +1,33 @@
 #!/bin/bash
 set -euo pipefail
 
-APP_NAME='mail-stats'
-COMMAND="${1:-usage}"
-BUILD_NUMBER="${2:-latest}"
+VERSION="${TRAVIS_BUILD_NUMBER}"
+BACKEND_IMAGE='rabblerouser/mail-stats'
+FRONTEND_IMAGE='rabblerouser/mail-stats-frontend'
+S3_DESTINATION="s3://rabblerouser-artefacts/frontends/mail-stats-frontend/${TRAVIS_BUILD_NUMBER}"
 
-function build() {
-  echo 'Building backend image'
-  cd ./backend && docker build --pull -t rabblerouser/${APP_NAME}:${BUILD_NUMBER} .
-  echo 'Building frontend image'
-  cd ../frontend && docker build --pull -t rabblerouser/${APP_NAME}-frontend:${BUILD_NUMBER} .
-  cd ..
-  echo 'Building frontend static assets'
-  docker rm assets || true
-  docker run --name assets rabblerouser/${APP_NAME}-frontend:${BUILD_NUMBER} yarn build
-  docker cp assets:/app/build assets
-}
+echo 'Building and testing backend'
+cd backend
+docker build --pull -t ${BACKEND_IMAGE} .
+docker tag ${BACKEND_IMAGE} ${BACKEND_IMAGE}:${VERSION}
+docker run ${BACKEND_IMAGE} yarn lint
+docker run ${BACKEND_IMAGE} yarn test
+cd ..
 
-function test() {
-  echo 'Running backend linting and tests'
-  docker run rabblerouser/${APP_NAME}:${BUILD_NUMBER} yarn lint
-  docker run rabblerouser/${APP_NAME}:${BUILD_NUMBER} yarn test
-  echo 'Running frontend linting and tests'
-  docker run rabblerouser/${APP_NAME}-frontend:${BUILD_NUMBER} yarn lint
-  docker run -e CI=true rabblerouser/${APP_NAME}-frontend:${BUILD_NUMBER} yarn test
-}
+echo 'Building and testing frontend'
+cd frontend
+docker build --pull -t ${FRONTEND_IMAGE} .
+docker tag ${FRONTEND_IMAGE} ${FRONTEND_IMAGE}:${VERSION}
+docker run ${FRONTEND_IMAGE} yarn lint
+docker run -e CI=true ${FRONTEND_IMAGE} yarn test
+docker run --name assets ${FRONTEND_IMAGE} yarn build
+docker cp assets:/app/build assets
+cd ..
 
-function push() {
-  echo 'Pushing backend image'
-  docker push rabblerouser/${APP_NAME}:${1}
-  echo 'Pushing frontend image (for dev purposes only)'
-  docker push rabblerouser/${APP_NAME}-frontend:${1}
-  echo 'Uploading assets to S3 artifact bucket'
-  aws s3 cp --recursive assets s3://rabblerouser-artefacts/frontends/${APP_NAME}-frontend/${1}
-}
-
-case "$COMMAND" in
-  'build')
-    build
-    ;;
-  'test')
-    test
-    ;;
-  'push')
-    docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
-    push ${BUILD_NUMBER}
-    if [[ ${BUILD_NUMBER} != 'latest' ]]; then
-      echo 'Also pushing artifacts as latest version'
-      docker tag rabblerouser/${APP_NAME}:${BUILD_NUMBER} rabblerouser/${APP_NAME}:latest
-      docker tag rabblerouser/${APP_NAME}-frontend:${BUILD_NUMBER} rabblerouser/${APP_NAME}-frontend:latest
-      push latest
-    fi
-    ;;
-  'usage')
-    echo 'Usage (`tag` defaults to latest):'
-    echo '  ./ci.sh build [tag]'
-    echo '  ./ci.sh test'
-    echo '  ./ci.sh push [tag]'
-    exit 1 # So that CI doesn't pass if no command is specified
-esac
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo 'Publishing backend and frontend'
+  docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
+  docker push ${BACKEND_IMAGE}
+  docker push ${FRONTEND_IMAGE}
+  aws s3 cp --recursive assets ${S3_DESTINATION}
+fi
